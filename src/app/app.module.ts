6a6d2297731d547a56fe4c02915f2c10e38f0566
Nataliia Dobrowolska <nmdobrowolska@gmail.com>
2020-09-30 10:09:45 +0200

    chnages to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
